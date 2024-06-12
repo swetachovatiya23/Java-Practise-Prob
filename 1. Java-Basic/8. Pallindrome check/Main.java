56fea60482266c285931560334071e3310a76446
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-12 10:47:28 -0400

    Finding pallindrome

--- 1. Java-Basic/8. Pallindrome check/Main.java
+++ 1. Java-Basic/8. Pallindrome check/Main.java
@@ -12,8 +12,22 @@ public class Main {
 
         //find the divisor to extract the first digit
         int divisor = 1;
+        while(number / divisor >= 10) {
+            divisor *= 10;
+        }
+        while(number != 0); // this line starts a loop that continues until the entire number is processed. 
 
+        int leading = number / divisor; //Extract leading digit
+        int trailing = number % 10; //extract trailing digit
 
+        if(leading != trailing) {
+            return false;
+        }
+
+        number = (number % divisor) / 10;
+        divisor /= 100;
+
+        return true;
     }
     public static void main(String args[]) {
         //scanner class
