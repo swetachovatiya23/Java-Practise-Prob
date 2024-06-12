7f45d8140102eb273cd7fc7d28754fce842c7e0f
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-12 08:42:19 -0400

    Finding sum of all digits

---
+++ 1. Java-Basic/6. Sum of digits/Main.java
@@ -0,0 +1,16 @@
+//Calculate the sum of the digits of a number.
+
+import java.util.*;
+
+public static int sumOfdigits(int a){
+
+}
+public static void main(String args[]){
+    //scanner class
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Enter the number : ");
+    int a = scanner.nextInt();
+
+    
+}
