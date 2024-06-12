02f7d082406f8e81c677045fa7f0edca9afe8389
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-12 10:39:15 -0400

    Finding pallindrome

---
+++ 1. Java-Basic/8. Pallindrome check/Main.java
@@ -0,0 +1,26 @@
+//Palindrome Check: Check if a number is a palindrome.
+
+import java.util.*;
+
+public class Main {
+
+    public static boolean isPlalindrome(int number) {
+        //negative numbers are not palindrome
+        if (number < 0) {
+            return false;            
+        }
+
+        //find the divisor to extract the first digit
+        int divisor = 1;
+
+
+    }
+    public static void main(String args[]) {
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number : ");
+        int num = scanner.nextInt();
+
+    }
+}
