696fecb9f6258dee11bc183c1ce9f32c2f9cd5c7
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-12 02:37:14 -0400

    sum of array elements

--- 2.Array_String/1.Array-sum/Main.java
+++ 2.Array_String/1.Array-sum/Main.java
@@ -1,7 +1,18 @@
 //calculate the sum of elementsin Array
 
-class Main{
+public class Main{
     public static void main(String args[]) {
-        
+        int[] arr = {1,2,3,4,5,6};
+
+        //1. initialise variable to store the value
+        int sum = 0;
+
+        //2. iterate 
+        for(int i=1; i<arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("sum : " + sum);
+
     }
 }
+
