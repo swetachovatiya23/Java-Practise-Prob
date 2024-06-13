2f567340baad7f03d23ad703c239344316daa107
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-13 10:07:44 -0400

    Calculating GCD and LCM

--- 1. Java-Basic/13. Sum of Natural numbers/Main.java
+++ 1. Java-Basic/13. Sum of Natural numbers/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static int sumOfnum(int num){
         int sum = 0;
 
-        for(int i=0; i<=num.length; i++){
+        for(int i=0; i<=num; i++){
             sum = sum + i;
         }
         return sum;
@@ -12,11 +12,16 @@ public class Main{
     }
     public static void main(String args[]){
         //scanner class
-        Scanner scanner = new Scanner(Sytem.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter the N natural num : ");
         int num = scanner.nextInt();
 
+        //print sum
+        int sumN = sumOfnum(num);
+
+        System.out.println("Sum of " + num + " is " + sumN);
+        scanner.close();
 
     }
 }
---
+++ 1. Java-Basic/14. GCD and LCM/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+
+    public static int gcd(int a, int b){
+        
+    }
+    public static void main(String args[]) {
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        int num1 = scanner.nextInt();
+
+        System.out.println(("Enter the second number : "));
+        int num2 = scanner.nextInt();
+
+
+    }
+}
