f084e9f3aa9ba4fc4a51072f730be7ccb3dce181
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-13 09:09:10 -0400

    cheking armsttrong number

--- 1. Java-Basic/12. Armstrong Number/Main.java
+++ 1. Java-Basic/12. Armstrong Number/Main.java
@@ -2,14 +2,26 @@
 
 import java.util.*;
 
-public class Main {
-    public static int armstrong(int num){
+public class Main{
 
+    public static boolean isArmstrong(int num){
+        int origionalNum = num;
+        int sum = 0;
+        int numberOfDigits = String.valueOf(num).length(); //chnaging num into string bcz we can only interate throught string not numbers
+
+        while( num != 0) {
+            int digit = num % 10; //to get the last digit of
+            sum += Math.pow(digit, numberOfDigits); // each digits of num will have the length of num as a power
+            num /=10; //to remove the last digit of num 
+        }
+        return sum == origionalNum;
     }
     public static void main(String args[]){
         //scanner class
         Scanner scanner = new Scanner(System.in);
+
         int num = scanner.nextInt();
+
 
     }
 }
