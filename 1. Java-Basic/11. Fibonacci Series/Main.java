fe13f16014ffb6410afa1ceb97b713312773fb39
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-13 05:18:47 -0400

    cal. fibonacci

---
+++ 1. Java-Basic/11. Fibonacci Series/Main.java
@@ -0,0 +1,18 @@
+// Fibonacci Series: Print the first N numbers of the Fibonacci series.
+
+import java.util.*;
+
+public class Main{
+    public static void fibonacci(int num){
+
+    }
+
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number to get it's fibonacci : ");
+        int num = scanner.nextInt();
+        
+    }
+}
