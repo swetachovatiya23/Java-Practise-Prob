8ead8c92df7828d5319bada985dfa3f6f648af72
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-13 05:09:48 -0400

    cal. factorial of number

--- 1. Java-Basic/10. Factorial Calculation/Main.java
+++ 1. Java-Basic/10. Factorial Calculation/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main{
     public static long factorial(int num) {
         long result = 1;
-        for(int i = 0; i<= num; i++) {
+        for(int i = 1; i<= num; i++) {
             result = result * i;
         }
         return result;
@@ -14,10 +14,16 @@ public class Main{
     public static void main(String args[]){
         //scanner class
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter number : ");
         int num = scanner.nextInt();
 
         //calculate fctorial
         long factorialsum = factorial(num);
+
+        System.out.println("Factorial of " + num + " is " + factorialsum);
+
+        scanner.close();
 
     }
 }
