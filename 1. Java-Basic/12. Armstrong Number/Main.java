0d5402132a961126fb5a1300f92264c00201b7db
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-13 08:00:31 -0400

    cheking armsttrong number

--- 1. Java-Basic/12. Armstrong Number/Main.java
+++ 1. Java-Basic/12. Armstrong Number/Main.java
@@ -1,1 +1,15 @@
 //Armstrong Number: Check if a number is an Armstrong number.
+
+import java.util.*;
+
+public class Main {
+    public static int armstrong(int num){
+
+    }
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+    }
+}
