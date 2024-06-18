31c72411ee4a5718e79a8bc63a52963f79b18575
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-18 14:29:06 -0400

    array sum

--- 2.Array_String/1.Array-sum/Main.java
+++ 2.Array_String/1.Array-sum/Main.java
@@ -4,18 +4,24 @@ import java.util.*;
 public class Main{
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
+
+        //Num of digits in array
         System.out.println("Enter the number of elements in array : ");
         int n = scanner.nextInt();
 
+        //array elements
         System.out.println("Enter the elemets of array : ");
         int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = scanner.nextInt();
+        }
 
 
         //1. initialise variable to store the value
         int sum = 0;
 
         //2. iterate 
-        for(int i=1; i<arr.length; i++) {
+        for(int i=0; i<n; i++) {
             sum += arr[i];
         }
         System.out.println("sum : " + sum);
