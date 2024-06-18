c2df1655aa1eac2a5664dd9e01c40cae307e3c4c
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-18 13:10:26 -0400

    Finding prime numbers from range

---
+++ 1. Java-Basic/16. Prime number in range/PrimeNumbersInRange.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class PrimeNumbersInRange {
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Method to print all prime numbers in a given range
+    public static void printPrimesInRange(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user for the range
+        System.out.print("Enter the start of the range: ");
+        int start = scanner.nextInt();
+        System.out.print("Enter the end of the range: ");
+        int end = scanner.nextInt();
+
+        // Print prime numbers in the given range
+        System.out.println("Prime numbers in the range " + start + " to " + end + ":");
+        printPrimesInRange(start, end);
+
+        scanner.close();
+    }
+}
