33c97314b573563f7a59efe33baaa9597a42f706
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-18 13:16:04 -0400

    Finding HCF of two numbers

---
+++ 1. Java-Basic/17. HCF of two numbers/Main.java
@@ -0,0 +1,20 @@
+//HCF of Two Numbers: Find the highest common factor (HCF) of two numbers.
+
+import java.util.*;
+
+public class Main{
+    public static int hcf(int num1, int num2){
+        
+    }
+    public static void main(String args[]) {
+        //scanner class
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter the second number : "); 
+        int num2 = sc.nextInt();
+
+    }
+}
