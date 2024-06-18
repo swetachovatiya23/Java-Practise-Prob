5f8b13253b43308767e47a498ef7ca1a8fe73e88
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-18 13:38:31 -0400

    Finding HCF of two numbers

--- 1. Java-Basic/17. HCF of two numbers/Main.java
+++ 1. Java-Basic/17. HCF of two numbers/Main.java
@@ -4,7 +4,12 @@ import java.util.*;
 
 public class Main{
     public static int hcf(int num1, int num2){
-        
+        if(num2 != 0){
+            int temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
     }
     public static void main(String args[]) {
         //scanner class
