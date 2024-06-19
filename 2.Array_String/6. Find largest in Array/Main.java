e138aac97bf4285ec6e1100efe247b1e97c06b75
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-19 14:26:36 -0400

    Largest element from Array

--- 2.Array_String/6. Find largest in Array/Main.java
+++ 2.Array_String/6. Find largest in Array/Main.java
@@ -7,7 +7,21 @@ public class Main{
 
     }
     public static void main(String args[]) {
-        
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt number of array elemt
+        System.out.println("Enter the number of array elements : ");
+        int n = scanner.nextInt();
+
+        //defininig Array of given size
+        int[] array = new int[n];
+
+        //prompt array element
+        System.out.println("Enter the elemts for array: ");
+        for(int i=0; i<n; i++){
+            array[i] = scanner.nextInt();
+        }
     }
 
 }
