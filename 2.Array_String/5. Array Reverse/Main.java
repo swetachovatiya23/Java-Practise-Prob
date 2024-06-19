12f336d7387602ad8478b4f51a5bc7d3381b4ddc
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-19 14:17:44 -0400

    array reverse

--- 2.Array_String/5. Array Reverse/Main.java
+++ 2.Array_String/5. Array Reverse/Main.java
@@ -3,6 +3,14 @@
 import java.util.*;
 
 public class Main{
+    public static void PrintArray(int[] arr){
+        for(int element : arr) {
+            System.out.println(element + " ");
+        }
+        System.out.println();
+    }
+
+
     public static void main(String args[]) {
         //scanner class
         Scanner scanner = new Scanner(System.in);
@@ -22,12 +30,12 @@ public class Main{
 
         //iterating over array and printing it's value
         int[] reversedArray = new int[n];  // new empty array
-        for(int i=3; i<n; i--){
+        for(int i=0; i<n; i++){
             reversedArray[i] = arr[n - 1 - i];  // revrse array and store it in new array
         }
 
         //print revrsed array
-        System.out.println("Reversed Array is : ");
+        System.out.print("Reversed Array is : ");
         PrintArray(reversedArray);
 
         scanner.close();
