c18f8ecb1a9505092f362527de2160e36d9420be
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-19 17:45:01 -0400

    Remove Duplicate  element from Array

--- 2.Array_String/9. Remove duplicate from Array/Main.java
+++ 2.Array_String/9. Remove duplicate from Array/Main.java
@@ -3,8 +3,24 @@
 import java.util.*;
 
 public class Main{
-    public static void duplicate(int[] array){
+    public static void removeDuplicate(int[] array){
+        int length = array.length;
 
+        for(int i=0; i<length; i++){
+            for(int j=i+1; j<length; j++){
+                if(array[i] == array[j]){
+                    
+                    //shift the element to overwrite duplicate
+                    for(int k = j; k < length-1; k++){
+                        array[k] = array[k + 1];
+                    }
+                    length--; //length will decrement
+                    
+                }else {
+                    newArray = array[i];
+                }
+            }
+        }
     }
     public static void main(String args[]){
         //scanner class
@@ -22,7 +38,7 @@ public class Main{
             array[i] = scanner.nextInt();
         }
 
-        
+
 
     }
 }
