d61a992b13b78e28acf56f5d86fc2a83a2548c92
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 15:08:38 -0400

    Matrix addition

---
+++ 2.Array_String/11. Matrix Multiplication/Main.java
@@ -0,0 +1,22 @@
+//Matrix multiplication
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+    //scanner class
+    Scanner scanner = new Scanner(System.in);
+
+    //prompt for dimensions of the matrix
+    System.out.println("Enter the number of rows : ");
+    int rows = scanner.nextInt();
+
+    System.out.println("Enter the number of columns : ");
+    int columns = scanner.nextInt();
+
+
+
+    }
+    //method to read the matrices from user
+    
+}
