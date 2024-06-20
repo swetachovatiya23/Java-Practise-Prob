7e6f17aab27147bedb66c2de7d092dc49dc24864
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 16:58:10 -0400

    Count the charecter in String

---
+++ 2.Array_String/13. Character count in string/Main.java
@@ -0,0 +1,13 @@
+//Charecter count in string
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]) {
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a String: ");
+        String str = scanner.nextLine();
+    }
+}
