2bdb2260bdc478831b0e5852cb13a743f4c4dedd
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 12:37:53 -0400

    Matrix addition

--- 2.Array_String/10. Matrix Addition/Main.java
+++ 2.Array_String/10. Matrix Addition/Main.java
@@ -21,7 +21,48 @@ public class Main{
         int[][] matrix2 = readMatrix(scanner, rows, columns);
 
         //Add the matricies
-        
+        int[][] resultMatrix = addMatrices(matrix1, matrix2, rows, columns);
+
+        //print the resulting matrix
+        System.out.println("Resulting Matrix after addition : ");
+        printMatrix(resultMatrix, rows, columns);
+
+        scanner.close();
+
+    }
+
+    //method to read a matrix from the user
+    public static int[][] readMatrix(Scanner scanner, int rows, int columns){
+        int[][] matrix = new int[rows][columns];
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    //method to add two matrices
+    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2, int rows, int columns) {
+        int[][] resultMatrix = new int[rows][columns];
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++) {
+                resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return resultMatrix;
+    }
+
+    //method to print a matrix
+    public static void printMatrix(int[][] matrix, int rows, int columns){
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
 
