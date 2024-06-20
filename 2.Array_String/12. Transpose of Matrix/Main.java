b97f0dab3d328e970c5ddc9bc4d947cc8aad43ae
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 16:26:57 -0400

    Transpose of Matrix

---
+++ 2.Array_String/12. Transpose of Matrix/Main.java
@@ -0,0 +1,22 @@
+//Transpose of matrix
+
+import java.util.*;
+
+public class Main {
+    pub
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt matrix size
+        System.out.println("Enter the number of rows : ");
+        int rows = scanner.nextInt();
+
+        System.out.println("Enter the numbers of columns : ");
+        int columns = scanner.nextInt();
+
+        //get the matrix from users 
+
+    }
+}
+
