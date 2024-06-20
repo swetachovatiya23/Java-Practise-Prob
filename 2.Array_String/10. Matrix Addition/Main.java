1b9f6acf5fa5e3b3966313df78be7a0242740326
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 10:51:29 -0400

    Remove Duplicate  element from Array

---
+++ 2.Array_String/10. Matrix Addition/Main.java
@@ -0,0 +1,17 @@
+//Matrix addition 
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        //prompt for dimentions of matrix
+        System.out.println("Enter the number of rows : ");
+        int rows = scanner.nextInt();
+        System.out.println("Enter the number of columns : ");
+        int column = scanner.nextInt();
+    }
+}
+
