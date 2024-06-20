aa69602983be46fba5ced6321cbdff374c2dbfac
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 15:24:21 -0400

    Matrix Multiplication

--- 2.Array_String/11. Matrix Multiplication/Main.java
+++ 2.Array_String/11. Matrix Multiplication/Main.java
@@ -60,7 +60,7 @@ public class Main{
     public static void printMatrix(int[][] matrix, int rows, int columns){
         for(int i=0; i<rows; i++){
             for(int j=0; j<columns; j++){
-                System.out.println(matrix[i][j] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
         }
         System.out.println();
