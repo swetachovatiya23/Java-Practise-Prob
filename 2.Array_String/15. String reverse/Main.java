51db856ccd7cc4c125ca948f9bb6754d059637d5
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 21:50:17 -0400

    String palindrome or not

---
+++ 2.Array_String/15. String reverse/Main.java
@@ -0,0 +1,14 @@
+//Reverse the string
+
+import java.util.*;
+
+public class Main{
+    public static String reverse(String str){
+        StringBuilder sb = new StringBuilder(str);
+        return sb.reverse().toString();
+    }
+
+    public static void main(String args[]){
+        
+    }
+}
