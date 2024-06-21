53311a1278793827394340e6f182faced7ccc4ec
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 21:40:40 -0400

    String palindrome or not

---
+++ 2.Array_String/14. String Palindrome/Main.java
@@ -0,0 +1,19 @@
+//String Palindrome
+
+import java.util.*;
+
+public class Main{
+    public static String palindrome(String str){
+        StringBuilder sb = new StringBuilder();
+        return sb.reverse().toString();
+    }
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the string : ");
+        String str = scanner.nextLine();
+
+       
+    }
+}
