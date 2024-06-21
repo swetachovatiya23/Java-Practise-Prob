6ddcf07d754ddb62bc0487b87337fa97607170e4
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-21 03:18:39 -0400

    String to Int conversion

---
+++ 2.Array_String/18. String to integer conversion/Main.java
@@ -0,0 +1,24 @@
+//String to Integer Conversion: Convert a string to an integer.
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the String represnting as integer : ");
+        String str = scanner.nextLine();
+
+        try {
+            //convert a string to an integer
+            int result = converStringToInt(str);
+
+            //output the result
+            System.out.println("The integer value is : " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("The Input is not a valid integer");
+        }
+        scanner.close();
+    }
+}
