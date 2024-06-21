b83fa8e642c45a1c542f1d5bb24e284ae05dba05
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-20 22:02:02 -0400

    check anagrams

---
+++ 2.Array_String/16. Anagram Chcek/Main.java
@@ -0,0 +1,18 @@
+//Check if two strings are anagrams.
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter first string : ");
+        String str1 = scanner.nextLine();
+
+        System.out.println("Enter second String : ");
+        String str2 = scanner.nextLine();
+
+        
+    }
+}
