d1330129abacc0200064412e91ea1dec3bff2bef
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-21 02:14:10 -0400

    Substring search

---
+++ 2.Array_String/17. Substring search/Main.java
@@ -0,0 +1,18 @@
+//Substring Search: Check if a substring exists in a string.
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the String : ");
+        String str = scanner.nextLine();
+
+        System.out.println("Enter the substring you want : ");
+        String subStr = scanner.nextLine();
+
+        
+    }
+}
