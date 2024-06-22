d55338acc47db9d3ce7abc359bccec779bba92b0
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-21 20:31:52 -0400

    Remove vowels from String

--- 2.Array_String/19. Remove Vowels/Main.java
+++ 2.Array_String/19. Remove Vowels/Main.java
@@ -3,6 +3,21 @@
 import java.util.*;
 
 public class Main{
+    public static String removeVowels(String str){
+        StringBuilder noVowels = new StringBuilder(); //using string builder to build result efficiently
+
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if(!isVowel(c)){
+                noVowels.append(c); //Append no vowel charecter
+            }
+        }
+        return 
+    }
+
+    public static boolean isVowel(char c){
+        return "aeiouAEIOU".indexof(c) != -1;
+    }
     public static void main(String args[]){
         //scanner class
         Scanner scanner = new Scanner(System.in);
@@ -11,6 +26,7 @@ public class Main{
         System.out.println("Enter the input String : ");
         String str = scanner.nextLine();
 
-        
+
+
     }
 }
