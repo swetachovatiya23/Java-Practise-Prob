c67bcb2be9052192b135680f6b6ff1490f7cb01f
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-06-21 20:27:33 -0400

    String to Int conversion

---
+++ 2.Array_String/19. Remove Vowels/Main.java
@@ -0,0 +1,16 @@
+//Remove Vowels: Remove all vowels from a string.
+
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        //scanner class
+        Scanner scanner = new Scanner(System.in);
+
+        //Prompt input string
+        System.out.println("Enter the input String : ");
+        String str = scanner.nextLine();
+
+        
+    }
+}
