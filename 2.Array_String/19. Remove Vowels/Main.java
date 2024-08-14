5b208ca2f27938156e577de37b6b096dbe3caa2c
swetachovatiya23 <swetachovatiya2312@gmail.com>
2024-08-14 12:46:58 -0400

    new changes

--- 2.Array_String/19. Remove Vowels/Main.java
+++ 2.Array_String/19. Remove Vowels/Main.java
@@ -33,10 +33,6 @@ public class Main{
         System.out.println("String after removing the vowels : " +result);
 
         scanner.close();
-
-        //Display the result
-        System.out.println("String after removing the vowels : " +result);
-
         
         
     }
